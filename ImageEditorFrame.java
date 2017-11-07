664e7141f05e93d3f21261f37c2b0afb358f209e
Tim <tim.dreger@gmx.de>
2017-11-07 17:47:18 +0100

    Created main, gitignore and imageEditorFrame

---
+++ ImageEditorFrame.java
@@ -0,0 +1,20 @@
+package base;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class ImageEditorFrame extends JFrame {
+
+	ImageEditorPanel panel;
+
+	public ImageEditorFrame() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 300);
+		createMenuBar();
+		setVisible(true);
+	}
+}
